b0d4427510ea674952c1f2d2cbae5068883f4888
aish-kdm2208 <aishkadam@gmail.com>
2025-07-31 01:47:52 +0530

    handle delimiter in input

--- calc_app.py
+++ calc_app.py
@@ -2,7 +2,15 @@ def add(string):
     if string=="":
         return 0
     
-    string = string.replace("\n", ",")
-    num=string.split(',')
+    elif string.startswith('//'):
+        delimiter = ';'
+        simple_string = string.replace('\n', delimiter)
+        list = simple_string.split(delimiter)
+        num=list[2:]
+
+    else:
+        string = string.replace("\n", ",")
+        num=string.split(',')
 
     return sum(int(i) for i in num)
+
