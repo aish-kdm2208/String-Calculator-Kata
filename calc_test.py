1c3337a297be077e8d8c056a8258b10e76857e75
aish-kdm2208 <aishkadam@gmail.com>
2025-07-31 01:20:23 +0530

    Add test_case for addtion of numbers

--- calc_test.py
+++ calc_test.py
@@ -6,5 +6,14 @@ class TestCalculator(unittest.TestCase):
         input=calc_app.add("")
         self.assertEqual(input, 0) 
 
+    def test_one_num(self):
+        input=calc_app.add("1")
+        self.assertEqual(input, 1)
+    
+    def test_addtion(self):
+        input=calc_app.add("1,2")
+        self.assertEqual(input, 3)
+    
+
 if __name__ == '__main__':
     unittest.main()
